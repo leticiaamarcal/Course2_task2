ca99e1efcc4838c60cafeada43770368c3baeebe
Letícia Russo <leticiaa.marcal@gmail.com>
2019-10-14 12:02:52 +0200

    update project survey

--- survey_project.R
+++ survey_project.R
@@ -120,13 +120,18 @@ C5TM_salary <- train(brand ~ salary,
                      trControl = crossV)
 
 #checar métrica
-C5TM_salary
+#Accuracy: 0.7245956    
+#Kappa: 0.4220858
 
 #predictions com o test
 Predi_Test_C5TM_salary <- predict(C5TM_salary, testing)
 
 #ver a accuracy no test
 confusionMatrix(testing$brand, Predi_Test_C5TM_salary)
+
+#métrica
+#Accuracy: 0.7324
+#Kappa: 0.4398            
 
 #set seed
 set.seed(123)
@@ -394,7 +399,7 @@ confusionMatrix(testing$brand, Predi_Test_SVMmodel)
 
 ######
 
-#agora vou treinar-NN
+#agora vou treinar k-NN
 
 #set seed
 set.seed(123)
@@ -470,7 +475,9 @@ confusionMatrix(testing$brand, Predi_Test_KNNmodel)
 
 #métricas
 #Accuracy: 0.9256          
-#Kappa: 0.8425       
+#Kappa: 0.8425    
+
+
 
 ######
 
@@ -539,7 +546,7 @@ ggplot() +
 
 #####
 
-#treinar pode 
+#treinar k-NN com todo os atributos para entender onde estariam os erros
 
 #set.seed
 set.seed(123)
@@ -584,7 +591,221 @@ ggplot() +
 #dei um run no knn (k=12) com salary e age e vou fazer o gráfico
 testing$colError <- abs(as.numeric(testing$brand) - as.numeric(Predi_Test_KNNmodel))
 
+
 #colocar a coluna colError (coluna gerada) como cor
 ggplot() +
   geom_jitter(data = testing, aes(x = age , y = salary,
                                  color = as.factor(colError)))
+
+###
+
+#dei um run no Single C5.0 Tree com salary e age e vou fazer o gráfico
+testing$coErrorTree <- abs(as.numeric(testing$brand) - as.numeric(Predi_Test_C5TM_salary_age))
+
+#colocar a coluna coErrorTree (coluna gerada) como cor
+ggplot() +
+  geom_jitter(data = testing, aes(x = age , y = salary,
+                                  color = as.factor(coErrorTree)))
+
+#encontrando patterns
+ggplot() +
+  geom_jitter(data = survey, aes(x = age , y = salary,
+                                  color = brand))
+
+###
+
+#dei um run Random Forest com salary e age e vou fazer o gráfico
+testing$errorRF <- abs(as.numeric(testing$brand) - as.numeric(Predi_Test_RFmodel))
+
+#colocar a coluna errorRF (coluna gerada) como cor
+ggplot() +
+  geom_jitter(data = testing, aes(x = age , y = salary,
+                                  color = as.factor(errorRF)))
+
+###
+
+#o modelo k-NN (k=12) foi escolhido. Agora vamos predict o dataset
+#que está incompleto
+
+#upload the data
+surveyIncomplete <- read.csv("C:/Users/letic/Desktop/SurveyIncomplete.csv")
+
+###
+
+#set seed
+set.seed(123)
+
+#vou fazer as previsões
+surveyIncomplete$brand <- predict(KNNmodel, surveyIncomplete)
+#colcoando o data set (surveyIcomplete) e cruzando com brand, eu 
+#substitui a coluna brand (que só tinha zero) pela minha prediction.
+#eu poderia criar uma coluna nova, mas o dataset ficaria com uma coluna 
+#extra e seria mais dificil unir com a tabela surveyComplete. 
+#a minha prediction agora é uma coluna e não um objeto
+
+#ver resultado
+summary(surveyIncomplete$brand)
+
+#resultado
+#0    1 
+#1915 3085
+
+set.seed(123)
+#a postResample é o mesmo que o confusion matrix: você vai comparar
+#métricas. Quanto o seu modelo acertou em relação a um dado que vc
+#já tem. Então no test (uma parte do seu dado), você compara
+#o dado que vc tem (a informação sobre o seu target) e aprevisão que
+#você fez naquele dado
+#Ground Truth é a target/label
+#confusion matrix dá mais informações que o postResample. P.e., dá o
+#recall
+postResample(testing$brand, pred = Predi_Test_KNNmodel)
+
+#result
+#Accuracy: 0.9223929
+#Kappa: 0.8353614 
+
+#eu quero exportar uma tabela/dataset. quero exportar o dataset
+#surveyIcomplete, mas que agora está com a coluna brand com as
+#previsões
+write.csv(surveyIncomplete, file = "Survey_Pred.csv")
+#Survey_Pred é o nome que eu escolhi para salvar o nome do arquivo
+#para ver onde está: getwd()
+#"C:/Users/letic/Documents"
+
+#eu quero juntar as duas tabelas e para isso usei a função rbind 
+#chamei a nova tabela de totalSurvey
+totalSurvey <- rbind(surveyIncomplete, survey)
+
+#checar
+totalSurvey
+str(totalSurvey)
+summary(totalSurvey$brand)
+
+#fazer gráfico
+ggplot() +
+  geom_jitter(data = totalSurvey, aes(x = age , y = salary,
+                                  color = brand))
+
+#o gráfico é muito parecido com o primeiro (traçado com o dataset
+#de 10k rows, sem os outros 5k). Isso significa que as patterns
+#do da dataset 10k (com as preferencias) são muito parecidas com os
+#as patterns do dataset de 5k (onde ão temos as preferências por marca).
+#por isso o modelo consegue prever tão bem. 
+
+ggplot() +
+  geom_density(data = surveyIncomplete,aes(age), 
+               fill = "red", alpha = 0.3) +
+  geom_density(data = survey, aes(age), 
+               fill = "blue", alpha = 0.3) +
+  theme_minimal()
+
+#####
+
+#all the metrics:
+#1-	Single C5.0 Tree - com todos os atributos (train)
+#Accuracy : 0.8469967
+#Kappa : 0.6832923
+
+#Metrics test
+#Accuracy : 0.9212 
+#Kappa : 0.833  
+---------
+#2-	Single C5.0 Tree com salary (train)
+#Accuracy: 0.7245956    
+#Kappa: 0.4220858
+
+#Metrics test:
+#Accuracy: 0.7324
+#Kappa: 0.4398            
+
+
+#3-	Single C5.0 Tree com salary and age (train)
+
+#Accuracy : 0.8975057
+#Kappa : 0.7841541
+
+#Metrics test
+#Accuracy : 0.9244                                
+#Kappa : 0.8387 
+
+#4-	Single C5.0 Tree com salary, age and zipcode (train)
+
+#Accuracy : 0.8699009
+#Kappa : 0.726712
+
+#Metrics test
+#Accuracy : 0.9228         
+#Kappa : 0.8363
+
+#5-	Single C5.0 Tree com salary, age , zipcode and credit (train)
+
+#Accuracy: 0.8697393   
+#Kappa: 0.7263673
+
+#Metrics test
+#Accuracy : 0.9228         
+#Kappa : 0.8363  
+
+#6-	Single C5.0 Tree com salary, age , zipcode, credit and car (train)
+
+#Accuracy: 0.8490426
+#Kappa: 0.6920992    
+
+#Metrics test
+#Accuracy : 0.9208          
+#Kappa : 0.832 
+--
+#7-	Random Forest with age and salary (train)
+
+#mtry=1 
+#Accuracy: 0.9142108
+#Kappa: 0.8180133
+
+#Metrics test
+#Accuracy : 0.9212 
+#Kappa : 0.8337 
+
+---
+#8-	SVM Linear with age and salary
+#Accuracy: 0.6217673
+#Kappa: 0
+
+#Metric test
+#Accuracy: 0.6217
+#Kappa: 0            
+
+--
+  
+#9-	K-NN with age and salary
+#k = 12
+#Accuracy : 0.9153819 
+#Kappa : 0.8204282 
+
+#Metrics test
+#Accuracy: 0.9256          
+#Kappa: 0.8425   
+
+--
+#10-	SVM Radial with age and salary
+#C     Accuracy   Kappa    
+#0.25  0.9202312  0.8305539
+#0.50  0.9218742  0.8340629
+#1.00  0.9227094  0.8360616
+
+#Metrics test
+#Accuracy: 0.9232         
+#Kappa: 0.8376    
+---
+#11-	 K-NN com todos os atributos
+#k  Accuracy   Kappa     
+#5  0.5504151  0.02176416
+#7  0.5626870  0.03049594
+#9  0.5765074  0.04709475
+
+#Metrics test
+#Accuracy : 0.5974  
+#Kappa : 0.0961 
+
+
+
